7fb4788560c43c4a39c7fa50daf198c33d9e8e96
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-07-16 19:22:32 -0300

    Database class refactored

--- src/models/Database.ts
+++ src/models/Database.ts
@@ -1,43 +1,137 @@
-import { MongoClient, Db } from 'mongodb';
+import { MongoClient, Db, ObjectId, WithId, OptionalUnlessRequiredId, Filter, Document, InsertOneResult } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 class Database {
-  private static instance: Database;
-  private db?: Db;
+    private url: string;
+    private dbName: string;
+    private client: MongoClient | null;
+    private db: Db | null;
 
-  private constructor() {
-    const url = process.env.MONGODB_URL!;
-    const dbName = process.env.MONGODB_DB_NAME!;
-
-    const client = new MongoClient(url);
-
-    client
-      .connect()
-      .then(() => {
-        this.db = client.db(dbName);
-        console.log('Conectado ao MongoDB');
-      })
-      .catch(error => {
-        console.error('Erro ao conectar ao MongoDB:', error);
-        process.exit(1);
-      });
-  }
-
-  public static getInstance(): Database {
-    if (!Database.instance) {
-      Database.instance = new Database();
+    constructor() {
+        this.url = process.env.MONGODB_URL!;
+        this.dbName = process.env.MONGODB_DB_NAME!;
+        this.client = null;
+        this.db = null;
     }
-    return Database.instance;
-  }
 
-  public getDB(): Db {
-    if (!this.db) {
-      throw new Error('Conexão com o banco de dados não estabelecida');
+    private async connect(): Promise<void> {
+        try {
+            this.client = await MongoClient.connect(this.url);
+            this.db = this.client.db(this.dbName);
+        } catch (error) {
+            console.error('Failed to connect to the database:', error);
+            throw error;
+        }
     }
-    return this.db;
-  }
+
+    private getDatabase(): Db {
+        if (!this.db) {
+            throw new Error('Database connection not established');
+        }
+        return this.db;
+    }
+
+    private async disconnect(): Promise<void> {
+        try {
+            if (this.client) {
+                await this.client.close();
+            }
+        } catch (error) {
+            console.error('Failed to disconnect from the database:', error);
+            throw error;
+        }
+    }
+
+    public async execute<T>(callback: (db: Db) => Promise<T>): Promise<T> {
+        try {
+            await this.connect();
+            const result = await callback(this.getDatabase());
+            await this.disconnect();
+            return result;
+        } catch (error) {
+            console.error('Failed to execute database operation:', error);
+            throw error;
+        }
+    }
+
+    public async find<T extends Document>(
+        collectionName: string,
+        filter: Filter<T> = {}
+    ): Promise<WithId<T>[]> {
+        return this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            const result = await collection.find(filter).toArray();
+            return result;
+        });
+    }
+    public async findOne<T extends Document>(
+        collectionName: string,
+        filter: Filter<T> = {}
+    ): Promise<WithId<T> | null> {
+        return this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            const result = await collection.findOne(filter);
+            return result;
+        });
+    }
+    // Exemplo de função para inserir um documento em uma coleção
+    public async insertOne<T extends Document>(
+        collectionName: string,
+        document: OptionalUnlessRequiredId<T>
+    ): Promise<InsertOneResult<T>> {
+        return await this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            return await collection.insertOne(document);
+        });
+    }
+
+    // Exemplo de função para inserir múltiplos documentos em uma coleção
+    public async insertMany<T extends Document>(
+        collectionName: string,
+        documents: OptionalUnlessRequiredId<T>[]
+    ): Promise<void> {
+        await this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            await collection.insertMany(documents);
+        });
+    }
+
+    // Exemplo de função para atualizar um documento em uma coleção
+    public async updateOne<T extends Document>(
+        collectionName: string,
+        filter: Filter<T>,
+        update: Partial<T>
+    ): Promise<void> {
+        await this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            await collection.updateOne(filter, { $set: update });
+        });
+    }
+
+    // Exemplo de função para excluir um documento de uma coleção
+    public async deleteOne<T extends Document>(
+        collectionName: string,
+        filter: Filter<T>
+    ): Promise<void> {
+        await this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            await collection.deleteOne(filter);
+        });
+    }
+
+    // Exemplo de função para buscar um documento por ID em uma coleção
+    public async findById<T extends Document>(
+        collectionName: string,
+        id: string
+    ): Promise<WithId<T> | null> {
+        return this.execute(async (db) => {
+            const collection = db.collection<T>(collectionName);
+            const result = await collection.findOne({ _id: new ObjectId(id) } as Filter<T>);
+            return result;
+        });
+    }
 }
 
 export default Database;
