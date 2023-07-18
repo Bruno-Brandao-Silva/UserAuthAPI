5a021fe01d045a74f39aa3a40057c0c25e83c75a
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-07-18 00:26:50 -0300

    Isolating Auth from User

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,38 +1,36 @@
 import { Router, Request, Response } from 'express';
 import User from '../models/User';
+import { verifyJWT } from '../middlewares/JWT';
 
 const router = Router();
 
-router.post('/register', async (req: Request, res: Response) => {
+router.get('/all', verifyJWT, async (req: Request, res: Response) => {
   try {
-    const { name, email, password } = req.body;
-
-    const id = await new User(name, email, password).register();
-
-    id ? res.status(201).json({ message: 'Usuário registrado com sucesso', id }) : new Error('Erro ao registrar usuário');
+    const users = await User.findAll();
+    res.status(200).json(users);
   } catch (error) {
-    console.error('Erro ao registrar usuário:', error);
+    console.error('Erro ao obter usuários:', error);
     res.status(500).end();
   }
 });
 
-router.post('/login', async (req: Request, res: Response) => {
+router.get('/:id', verifyJWT, async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
+    const { id } = req.params;
+    const user = await User.findById(id);
 
-    const user = await User.login(email, password);
     if (user) {
-      res.status(200).json({ message: 'Usuário autenticado com sucesso' });
+      res.status(200).json(user);
     } else {
-      res.status(401).end();
+      res.status(404).end();
     }
   } catch (error) {
-    console.error('Erro ao autenticar usuário:', error);
+    console.error('Erro ao obter usuário:', error);
     res.status(500).end();
   }
 });
 
-router.put('/:id', async (req: Request, res: Response) => {
+router.put('/:id', verifyJWT, async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { name, email, password } = req.body;
@@ -48,7 +46,7 @@ router.put('/:id', async (req: Request, res: Response) => {
 
 });
 
-router.delete('/:id', async (req: Request, res: Response) => {
+router.delete('/:id', verifyJWT, async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
 
@@ -57,32 +55,6 @@ router.delete('/:id', async (req: Request, res: Response) => {
     res.status(200).json({ message: 'Usuário excluído com sucesso' });
   } catch (error) {
     console.error('Erro ao excluir usuário:', error);
-    res.status(500).end();
-  }
-});
-
-router.get('/all', async (req: Request, res: Response) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Erro ao obter usuários:', error);
-    res.status(500).end();
-  }
-});
-
-router.get('/:id', async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findById(id);
-
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).end();
-    }
-  } catch (error) {
-    console.error('Erro ao obter usuário:', error);
     res.status(500).end();
   }
 });
