c38ee7ff53fdf4c77c6506333bb72acca24c6c8f
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-07-17 23:59:00 -0300

    Add cookie parser & jwt

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import bodyParser from 'body-parser';
 import UserRoute from './routes/User';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(cors());
 app.use('/api/user', UserRoute);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
