327d29b2f6ff4890d37c3135b5510522dab38abc
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-07-23 23:10:22 -0300

    Email has to be unique

--- src/models/User.ts
+++ src/models/User.ts
@@ -17,13 +17,20 @@ class User {
         this.password = password;
     }
 
-    async register(): Promise<ObjectId | null> {
-        const result = await User.db.insertOne(User.collection, {
-            name: this.name,
-            email: this.email,
-            password: this.password,
-        });
-        return result.acknowledged ? result.insertedId : null;
+    async register(): Promise<ObjectId | void> {
+        const user = await User.db.findOne(User.collection, { email: this.email });
+        if (!user) {
+            const result = await User.db.insertOne(User.collection, {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+            });
+            if (result.acknowledged)
+                return result.insertedId
+            else throw new Error('Erro ao inserir usuário');
+        } else {
+            throw new Error('Usuário já existe');
+        }
     }
 
     async update(): Promise<void> {
@@ -34,21 +41,20 @@ class User {
         await User.db.deleteOne(User.collection, { _id: new ObjectId(id) });
     }
 
-    static async findById(id: string): Promise<User | null> {
-
+    static async findById(id: string): Promise<User | void> {
         const user = await this.db.findById(User.collection, id);
         if (user) {
             return new User(user.name, user.email, user.password, user._id);
         }
-        return null;
     }
-    static async login(email: string, password: string): Promise<User | null> {
+
+    static async login(email: string, password: string): Promise<User | void> {
         const user = await this.db.findOne(User.collection, { email, password });
         if (user) {
             return new User(user.name, user.email, user.password, user._id);
         }
-        return null;
     }
+    
     static async findAll(): Promise<User[]> {
         const users = await this.db.find(User.collection);
         return users.map(user => new User(user.name, user.email, user.password, user.id));
