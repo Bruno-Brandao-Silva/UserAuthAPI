524fb964c4de04e8584ed422449e4f21ba63c2d8
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-07-23 23:08:02 -0300

    Moving back Auth to User

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,36 +1,76 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import User from '../models/User';
-import { verifyJWT } from '../middlewares/JWT';
+import { verifyJWT, signJWT } from '../middlewares/JWT';
+import { validationResult } from 'express-validator';
 
 const router = Router();
 
-router.get('/all', verifyJWT, async (req: Request, res: Response) => {
+router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Erro ao obter usuários:', error);
-    res.status(500).end();
+    const { name, email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const id = await new User(name, email, password).register();
+    res.status(201).json({ message: 'Usuário registrado com sucesso', id });
+  } catch (e: any) {
+    console.error('Erro ao registrar usuário:', e);
+    if (e.message === 'Usuário já existe')
+      res.status(409).json({ error: 'Usuário já existe' });
+    else
+      res.status(500).json({ error: 'Erro ao registrar usuário' });
   }
 });
 
-router.get('/:id', verifyJWT, async (req: Request, res: Response) => {
+router.post('/login', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.login(email, password);
+    if (user) {
+      const token = signJWT(user);
+      res.cookie('token', token, { httpOnly: true, secure: true, expires: new Date(Date.now() + 1000 * 60 * 60) });
+      res.status(200).json({ message: 'Usuário autenticado com sucesso' });
+    } else {
+      res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+  } catch (error) {
+    console.error('Erro ao autenticar usuário:', error);
+    res.status(500).json({ error: 'Erro ao autenticar usuário' });
+  }
+});
+
+
+router.get('/all', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await User.findAll();
+    res.json({ data: users });
+  } catch (error) {
+    console.error('Erro ao obter usuários:', error);
+    next(error);
+  }
+});
+
+router.get('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
 
     if (user) {
-      res.status(200).json(user);
+      res.json({ data: user });
     } else {
       res.status(404).end();
     }
   } catch (error) {
     console.error('Erro ao obter usuário:', error);
-    res.status(500).end();
+    next(error);
   }
 });
 
-router.put('/:id', verifyJWT, async (req: Request, res: Response) => {
+router.put('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
     const { name, email, password } = req.body;
@@ -38,24 +78,23 @@ router.put('/:id', verifyJWT, async (req: Request, res: Response) => {
     const user = new User(name, email, password, id);
     await user.update();
 
-    res.status(200).json({ message: 'Usuário atualizado com sucesso' });
+    res.json({ message: 'Usuário atualizado com sucesso' });
   } catch (error) {
     console.error('Erro ao atualizar usuário:', error);
-    res.status(500).end();
+    next(error);
   }
-
 });
 
-router.delete('/:id', verifyJWT, async (req: Request, res: Response) => {
+router.delete('/:id', verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
 
-    const user = await User.delete(id);
+    await User.delete(id);
 
-    res.status(200).json({ message: 'Usuário excluído com sucesso' });
+    res.json({ message: 'Usuário excluído com sucesso' });
   } catch (error) {
     console.error('Erro ao excluir usuário:', error);
-    res.status(500).end();
+    next(error);
   }
 });
 
